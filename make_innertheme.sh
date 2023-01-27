3d469d5a1be8b7e2dc5d2c7d176d95e163bbb1ce
Zaph-x <35431127+Zaph-x@users.noreply.github.com>
2023-01-27 09:50:08 +0100

    Replaced `globalscale` in favour of `1` which causes background and logo to render properly

--- make_innertheme.sh
+++ make_innertheme.sh
@@ -8,7 +8,7 @@ echo "
 \def\insertbackgroundimg{%
 \resizebox{1.05\paperwidth}{1.05\paperheight}{
 " >> beamerinnerthemeAAU.sty
-svg2tikz assets/background.svg | sed -n '/tikzpicture/,/tikzpicture/p' | sed -e 's/c29224e/aau@blue1/g' >> beamerinnerthemeAAU.sty
+svg2tikz assets/background.svg | sed -n '/tikzpicture/,/tikzpicture/p' | sed -e 's/c29224e/aau@blue1/g' | sed -e 's/\\globalscale/1/g' >> beamerinnerthemeAAU.sty
 echo "}}" >> beamerinnerthemeAAU.sty
 
 
@@ -18,5 +18,5 @@ echo "
 \definecolor{cffffff}{RGB}{255,255,255}
 \resizebox{\graphicswidth}{!} {%
 " >> beamerinnerthemeAAU.sty
-svg2tikz assets/logo.svg | sed -n '/tikzpicture/,/tikzpicture/p' >> beamerinnerthemeAAU.sty
+svg2tikz assets/logo.svg | sed -n '/tikzpicture/,/tikzpicture/p' | sed -e 's/\\globalscale/1/g' >> beamerinnerthemeAAU.sty
 echo "}}" >> beamerinnerthemeAAU.sty
